ff1d0d491a11fb2be6ef3ca6d4bd10e5e342c898
kbys <work4kbys@gmail.com>
2018-09-03 16:01:02 +0900

    source_me.sh can runs anywhere

--- source_me.sh
+++ source_me.sh
@@ -1,3 +1,10 @@
 
+pushd `dirname "$BASH_SOURCE"` > /dev/null
+
 export BI_BUILDER_ROOT=`pwd`
 export PATH=$BI_BUILDER_ROOT/build/host/bin:$PATH
+
+echo BI_BUILDER_ROOT=$BI_BUILDER_ROOT
+echo PATH=$PATH
+
+popd > /dev/null
